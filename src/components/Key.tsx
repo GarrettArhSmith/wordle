4fe53b6b283d4b5b3339221c532b7734a88d5acf
Garrett Smith <garrettarhsmith@gmail.com>
2024-07-07 18:12:06 -0400

    added hover and active states for keys

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -13,7 +13,8 @@ const Key = ({ char, children, onClick, used }: Props) => {
       onClick={onClick}
       className={`h-14 flex items-center justify-center rounded uppercase font-bold
         ${char ? "flex-1 min-w-6 max-w-11" : "w-14 md:w-16 text-sm"}
-        ${used ? "bg-gray-500 text-white" : "bg-gray-300"}`}
+        ${used ? "bg-gray-500 text-white" : "bg-gray-300"}
+        hover:brightness-90 active:brightness-75`}
     >
       {char ? char : children}
     </button>
